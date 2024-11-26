8b0ad7d715eb76780d93691357bdc4d49487a746
TanTzeWei <S10266915@connect.np.edu.sg>
2024-11-26 12:50:14 +0800

    just updating abit of java

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-// Get modal elements
+
 const modal = document.getElementById("sign-in-modal");
 const openModalBtn = document.getElementById("open-modal");
 const closeModalBtn = document.getElementById("close-modal");
 
-// Open modal
+
 openModalBtn.addEventListener("click", () => {
     modal.style.display = "flex"; // Flex for centering
 });
 
-// Close modal
+
 closeModalBtn.addEventListener("click", () => {
     modal.style.display = "none";
 });
 
-// Close modal when clicking outside content
+
 window.addEventListener("click", (e) => {
     if (e.target === modal) {
         modal.style.display = "none";
     }
 });
-
 
 
 
